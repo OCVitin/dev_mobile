3e41cd37f6d66f0acd0d5dd9fcb3bd0d76cbf727
Vito <103535367+OCVitin@users.noreply.github.com>
2023-04-28 10:43:55 -0300

    Update cardCompra.js

--- CinemaApp/src/componentes/cardCompra.js
+++ CinemaApp/src/componentes/cardCompra.js
@@ -8,21 +8,21 @@ export default function Card({props}){
 
     return (
         <View style={styles.container}>
-            <View style={styles.campoImagem}>
+            <View style={styles.poster}>
                 <Image style={styles.imagem} source={{uri: baseURL + props.attributes.poster.data.attributes.url}}/>
             </View>
-            <View style={styles.infoItem}>
+            <View style={styles.infoFilme}>
                 <View>
-                    <Text style={styles.nomeItem}>{props.attributes.titulo}</Text>
+                    <Text style={styles.titulo}>{props.attributes.titulo}</Text>
                 </View>
                 <View>
-                    <Text style={styles.nomeItem}>{props.attributes.subtitulo}</Text>
+                    <Text style={styles.subtitulo}>{props.attributes.subtitulo}</Text>
                 </View>
                 <View>
-                    <Text style={styles.descItem}>{props.attributes.sinopse}</Text>
+                    <Text style={styles.sinopse}>{props.attributes.sinopse}</Text>
                 </View>
                 <View style={styles.btnTroca}>
-                    <Button title="Trocar!"/>
+                    <Button title="Cromprar!" color="#950101"/>
                 </View>
             </View>
         </View>
