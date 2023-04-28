fb1f3836673552e47681a5c492fe244decba51e6
Vito <103535367+OCVitin@users.noreply.github.com>
2023-04-28 10:43:16 -0300

    Update App.js

--- CinemaApp/App.js
+++ CinemaApp/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Card from './src/componentes/cardCompra';
 
 export default function App(){
@@ -20,10 +20,18 @@ export default function App(){
 
   return (
     <View style={styles.container}>
-      {filmes.length > 0 ? 
-      filmes.map(filme => <Card props={filme}/>) : 
-      <Text>Carregando...</Text>}
-      <StatusBar style="auto"/>
+      <View style={styles.header}>
+        <Text style={styles.logo}>OCineminha</Text>
+        <View style={styles.barraPesq}>
+          <Text style={styles.buscar}>Buscar mais filmes...</Text>
+        </View>
+      </View>
+      <View style={styles.catalogo}>
+        {filmes.length > 0 ? 
+        filmes.map(filme => <Card props={filme}/>) : 
+        <Text>Carregando...</Text>}
+        <StatusBar style="auto"/>
+      </View>
     </View>
   );
 }
@@ -31,8 +39,36 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#101010',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#950101',
+    padding: 10,
+    flexDirection: 'row',
+    width: '100%',
+    position: 'sticky',
+    top: 0,
+    zIndex: 1,
+  },
+  logo: {
+    color: '#fefefe',
+    fontWeight: 'bold',
+    width: '30%',
+    fontSize: 17,
+    margin: 2,
+  },
+  barraPesq: {
+    backgroundColor: '#750101',
+    width: '70%',
+    padding: 5,
+    borderRadius: 4,
+  },
+  buscar: {
+    color: '#000000',
+    opacity: 0.4,
+  },
+  catalogo: {
+    margin: 6,
   },
 });
