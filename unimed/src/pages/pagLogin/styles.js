2b5b9eb838532dbb844214f2544158c7f553d6d2
Vito <103535367+OCVitin@users.noreply.github.com>
2023-06-01 22:22:31 -0300

    Update styles.js

--- unimed/src/pages/pagLogin/styles.js
+++ unimed/src/pages/pagLogin/styles.js
@@ -6,7 +6,25 @@ const styles = StyleSheet.create({
         height: '100%',
         alignItems: 'center',
         justifyContent: 'center',
-        // backgroundColor: 'green', tem que colocar gradiente
+        backgroundColor: '#009859',
+    },
+    input: {
+        width: 250,
+        marginTop: 40,
+        padding: 10,
+        fontSize: 18,
+        color: 'rgba(0, 152, 89, 0.5)',
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#009859'
+    },
+    btnEntrar: {
+        width: 250,
+        marginTop: 40,
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#ffffff'
     },
 });
 
