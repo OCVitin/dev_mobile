5b8f391ea01f1e9a725ddd2c83a61bd87a469006
Vito <103535367+OCVitin@users.noreply.github.com>
2023-06-02 17:07:27 -0300

    Delete unimed directory

--- unimed/src/components/logoUnimed/index.js
+++
@@ -1,8 +0,0 @@
-import { Image } from "react-native";
-import styles from "./styles.js";
-
-export default function Logo(){
-    return (
-        <Image style={styles.imagem} source={{uri: 'https://logodownload.org/wp-content/uploads/2014/05/unimed-logo-1.png'}}/>
-    )
-}
